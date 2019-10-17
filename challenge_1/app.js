2b718b00b63f46edd44ea91da74f9677ed6752c4
Manuela Gomez <mmanu.gomez@gmail.com>
2019-10-17 08:03:18 -0400

    [wip] BoardView handles click event

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,3 +1,47 @@
-alert("Using Javascript");
+let doc = window.document;
+HTMLDocument.prototype.Sel = function(selector) {
+  // Only for HTML
+  return this.querySelector(selector);
+};
 
+var App = {
+  init: () => {
+    BoardView.init();
+  }
+};
 
+var BoardView = {
+  init: () => {
+    BoardView.clearBoard();
+    BoardView.readyPlay();
+  },
+  onBoard: callback => {
+    // the reference to this.table seems problematic
+    var cells = this.table.querySelectorAll("div.box");
+    cells.forEach(cell => {
+      callback(cell);
+    });
+  },
+  clearBoard: () => {
+    BoardView.onBoard(cell => {
+      cell.innerHTML = "";
+    });
+  },
+  readyPlay: () => {
+    BoardView.onBoard(cell => {
+      cell.addEventListener("click", event => {
+        BoardView.makePlay(cell);
+      });
+    });
+  },
+  makePlay: (cell) => {
+    cell.innerHTML = "X";
+  }
+};
+
+var Board = {
+  state: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+  init: () => {
+
+  }
+}
