f3beaf93079ae465ba0f72c28fb3025ded29f4bd
Manuela Gomez <mmanu.gomez@gmail.com>
2019-10-18 18:26:46 -0400

    [wip] Starting a Node Project: package.json

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,9 @@
-
+// Starting a Node Project: package.json
+// Serving up index.html and Assets
+// HTML Forms
+// Submitting Form Data using the Default Action
+// Server Responses based on Form Submission
+// Submitting Data via AJAX
+// Preventing Page Reload
+// Receiving Data as a Result of an AJAX Request
 
