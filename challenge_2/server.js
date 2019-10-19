52ff8a05976be36615a78b883523b916d90e3a9b
Manuela Gomez <mmanu.gomez@gmail.com>
2019-10-19 11:50:10 -0400

    [wip] Parses data coming from form

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.listen(3000);
 
 app.use(express.static('client'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/json', (req, res, next) => {
-  console.log(req);
+  console.log(req.body);
   res.redirect('/');
   next();
 });
