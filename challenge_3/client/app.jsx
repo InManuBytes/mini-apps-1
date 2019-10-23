016271dc16effd1aa733dd57e1dcefb2657920b7
Manuela Gomez <mmanu.gomez@gmail.com>
2019-10-23 08:18:21 -0400

    [wip] Challenge 3 - Multi step checkout: using App and rendering with react, importing correctly to html

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,16 @@
+// create a class based app
+import React from 'react'
 
+class App extends React.component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 'home' // 1. home 2. form1 3. form2 4. form3 5. summary
+    }
+  }
+  render () {
+    return 'some jsx'
+  }
+}
 
+ReactDOM.render(<App />, document.getElementById("app"));
--- challenge_3/pulic/app.js
+++ challenge_3/pulic/app.js
@@ -1,2 +1,20 @@
+// create a class based app
+import React from 'react';
 
+class App extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 'home' // 1. home 2. form1 3. form2 4. form3 5. summary
+
+    };
+  }
+
+  render() {
+    return 'some jsx';
+  }
+
+}
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
 //# sourceMappingURL=app.js.map
