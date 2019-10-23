4c175c2227ff4f3a3caded3b5f1faf934e48f75e
Manuela Gomez <mmanu.gomez@gmail.com>
2019-10-22 22:34:14 -0400

    [wip] Challenge 3 - Multi step checkout: Serving files from Express

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+// we want to serve our JS files in the public folder,
+// where Babel is placing the transpiled files.
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
