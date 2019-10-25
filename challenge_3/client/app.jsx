b8b196f4357cacb00d2035702a76291f9a827ca3
Manuela Gomez <mmanu.gomez@gmail.com>
2019-10-24 21:33:22 -0400

    [wip] Challenge 3 - Button renders checkout on homepage

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,16 +1,32 @@
 // create a class based app
-import React from 'react'
 
-class App extends React.component{
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      step: 'home' // 1. home 2. form1 3. form2 4. form3 5. summary
-    }
+      step: 'home', // 1. home 2. form1 3. form2 4. form3 5. summary
+    };
   }
-  render () {
-    return 'some jsx'
+
+  render() {
+    return (
+      <div>
+        <Button step={this.state.step} />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const Button = (state) => {
+  let value;
+  if (state.step === 'home') {
+    value = 'Checkout';
+  } else {
+    value = 'Button';
+  }
+  return (
+    <button>{value}</button>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // middleware
-const cors = requre('cors')
+//const cors = require('cors')
 
 // we want to serve our JS files in the public folder,
 // where Babel is placing the transpiled files.
-app.use(express.static('public'))
-app.use(cors());
+app.use(express.static(__dirname + '/public'));
+//app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
