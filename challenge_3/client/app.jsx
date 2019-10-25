72aa3dfa0bb33c9d2178121010656c8eed8b38eb
Manuela Gomez <mmanu.gomez@gmail.com>
2019-10-24 23:13:37 -0400

    [wip] Challenge 3 - Multi step checkout: Button render for different states

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -4,29 +4,57 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      step: 'home', // 1. home 2. form1 3. form2 4. form3 5. summary
+      steps: {1: 'checkout', 2: 'form1', 3: 'form2', 4: 'form3', 5: 'summary'},
+      currStepNum: 1
     };
+
+    this.ClickForNextStep = this.ClickForNextStep.bind(this);
+  }
+
+  ClickForNextStep() {
+    this.setState(state => {
+      return {currStepNum: (state.currStepNum !== 5) ? state.currStepNum + 1 : 1};
+    });
   }
 
   render() {
     return (
       <div>
-        <Button step={this.state.step} />
+        <Button step={this.state.currStepNum} form={this.state.steps[this.state.currStepNum]} nextStep={this.ClickForNextStep} />
       </div>
     );
   }
 }
 
-const Button = (state) => {
+const Button = ({step, form, nextStep}) => {
   let value;
-  if (state.step === 'home') {
-    value = 'Checkout';
+  let type;
+  console.log('State step:', step, 'form:', form);
+  if (step === 1 || step === 5) {
+    type = 'button';
+    if (step === 1) {
+      value = 'Checkout';
+    } else {
+      value = 'Purchase';
+    }
   } else {
-    value = 'Button';
+    value = 'Next';
+    type = 'submit';
   }
   return (
-    <button>{value}</button>
+    <button type={type} id={form} onClick={nextStep} >{value}</button>
   );
 };
+
+// ERROR: cannot read property number of undefined
+// Button.propTypes = {
+//   step: React.PropTypes.number.isRequired,
+//   form: React.PropTypes.string.isRequired
+// };
+
+const Form = (props) => {
+
+
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
